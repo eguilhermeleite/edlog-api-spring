f9005813080d8fe5c30aafce11018fe7c902adc0
Edvaldo G Leite <eguilhermeleite@gmail.com>
2023-03-18 21:28:19 -0300

    Projeto criado com starter-web e devtools

---
+++ edlog-api-spring/src/main/java/com/edlog/api/EdlogApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edlog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdlogApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdlogApiSpringApplication.class, args);
+	}
+
+}
